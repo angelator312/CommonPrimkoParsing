9827e447319dfff82112f9f612f427ed0c9d1240
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-22 13:25:08 +0300

    added level editor types to be in the common module

---
+++ LevelEditorTypes.gd
@@ -0,0 +1,10 @@
+class_name LevelEditorTypes
+
+enum PropertyTypes {
+	position = 0,
+	tile_map_data = 1,
+	curve_points,
+}
+
+const QUDRANT_COMPRESS: FileAccess.CompressionMode = FileAccess.COMPRESSION_FASTLZ
+const MAX_BUFFER_SIZE = 1e5
