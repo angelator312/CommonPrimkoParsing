301377228dfb35911bc07fed2ed3a4f65acb1c69
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-28 06:28:21 +0300

    not working coin scene

---
+++ GameObjects/Coin.gd
@@ -0,0 +1,21 @@
+# How to add new game object
+# - Coin:the name of the game object, but in PascalCase (like `Checkpoint`)
+# - coin:Coin but in snake_case (like `checkpoint`)
+
+static func load(object_str: String, root: Node2D):
+	if object_str.begins_with("coin"):
+		from_string(object_str, root)
+		return true
+	return false
+
+static func from_string(encodedObject: String, root: Node2D):
+	print("coin:", encodedObject)
+	encodedObject = encodedObject.trim_prefix("coin type=\"Coin\"]\n")
+	var props = CommonParsingFormat1.split_props(encodedObject)
+	print("props:", props)
+	add_node(props, root)
+
+static func add_node(properties: Dictionary[String, Variant], root: Node2D):
+	var node: Node2D = CommonLoadFormat1.config.CoinScene.instantiate()
+	print("coin name:", node.name) # Only for debug purposes
+	CommonLoadFormat1.add_child(node, root, properties)
