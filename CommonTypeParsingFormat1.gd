11d136c06e80a548da9678120aa1d0b268b7aa0d
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-28 05:17:59 +0300

    [better code] Moved get moved to func some code

--- CommonTypeParsingFormat1.gd
+++ CommonTypeParsingFormat1.gd
@@ -21,18 +21,7 @@ static func parse_property(str: String) -> Variant:
 	
 	if str.begins_with("Array<Vector2>"):
 		str = str.trim_prefix("Array<Vector2>(").trim_suffix(")")
-		var str_split = str.split(",", false)
-		var arr: Array[Vector2] = []
-		var prev: float = INF
-		for e: String in str_split:
-			e = e.trim_prefix(" ").trim_prefix("(")
-			e = e.trim_suffix(" ").trim_suffix(")")
-			if prev != INF:
-				arr.append(Vector2(prev, float(e)))
-				prev = INF
-			else:
-				prev = float(e)
-		return arr
+		return parse_array_of_vector2(str)
 	
 	if str.begins_with("TileMapLayerFunctions"):
 		return TileMapLayerFunctions.parse_tile_map_layer_functions(str)
@@ -56,5 +45,19 @@ static func parse_node_path_string(str: String) -> NodePath:
 	str = str.trim_prefix("NodePath(").trim_suffix(")")
 	return NodePath(str)
 
+
+static func parse_array_of_vector2(str: String):
+	var str_split = str.split(",", false)
+	var arr: Array[Vector2] = []
+	var prev: float = INF
+	for e: String in str_split:
+		e = e.trim_prefix(" ").trim_prefix("(")
+		e = e.trim_suffix(" ").trim_suffix(")")
+		if prev != INF:
+			arr.append(Vector2(prev, float(e)))
+			prev = INF
+		else:
+			prev = float(e)
+	return arr
 
 # Add new parse_* functions ↓↓↓
