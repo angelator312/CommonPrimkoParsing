0ea3987fde02afc3cf208c9caeb33340a962e3d6
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-09-28 07:06:33 +0300

    added Checkpoint

---
+++ GameObjects/Checkpoint.gd
@@ -0,0 +1,23 @@
+extends CommonGameObject
+
+# How to add new game object
+# - Checkpoint:the name of the game object, but in PascalCase (like `Checkpoint`)
+# - checkpoint:Checkpoint but in snake_case (like `checkpoint`)
+
+static func load(object_str: String, root: Node2D):
+	if object_str.begins_with("checkpoint"):
+		_from_string(object_str, root)
+		return true
+	return false
+
+static func _from_string(encodedObject: String, root: Node2D):
+	print("checkpoint:", encodedObject)
+	encodedObject = encodedObject.trim_prefix("checkpoint type=\"Checkpoint\"]\n")
+	var props = CommonParsingFormat1.split_props(encodedObject)
+	print("props:", props)
+	_add_node(props, root)
+
+static func _add_node(properties: Dictionary[String, Variant], root: Node2D):
+	var node: Node2D = CommonLoadFormat1.config.CheckpointScene.instantiate()
+	print("checkpoint name:", node.name) # Only for debug purposes
+	CommonLoadFormat1.add_child(node, root, properties)
