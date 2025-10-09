c030a414fdfa4be280a3f6eae5a607d09787fbab
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-10-09 06:09:43 +0300

    some refactoring

--- ResourceTemplates/CommonObject.gd
+++ ResourceTemplates/CommonObject.gd
@@ -1,24 +1,24 @@
 @abstract class_name CommonGameObject
 
 # How to add new game object
-# - `Name`:the name of the game object, but in PascalCase (like `Checkpoint`)
-# - `name`:`Name` but in snake_case (like `checkpoint`)
+# - Name:the name of the game object, but in PascalCase (like Checkpoint)
+# - name:Name but in snake_case (like checkpoint)
 
 static func load(object_str: String, root: Node2D):
-	if object_str.begins_with("`name`"):
+	if object_str.begins_with("name"):
 		_from_string(object_str, root)
 		return true
 	return false
 
 static func _from_string(encodedObject: String, root: Node2D):
-	MyLogger.obj("`name`:", encodedObject)
-	encodedObject = encodedObject.trim_prefix("`name` type=\"`Name`\"]\n")
+	MyLogger.obj("name:", encodedObject)
+	encodedObject = encodedObject.trim_prefix("name type=\"Name\"]\n")
 	var props = CommonParsingFormat1.split_props(encodedObject)
 	MyLogger.props(props)
 	_add_node(props, root)
 
 static func _add_node(properties: Dictionary[String, Variant], root: Node2D):
-	#var node: Node2D = CommonLoadFormat1.config.`Name`Scene.instantiate()
-	#print("`name` name:", node.name) # Only for debug purposes
+	#var node: Node2D = CommonLoadFormat1.config.NameScene.instantiate()
+	#print("name name:", node.name) # Only for debug purposes
 	#CommonLoadFormat1.add_child(node, root, properties)
 	pass
